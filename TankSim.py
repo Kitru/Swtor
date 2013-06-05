9989e7011896b9fe86004770862a504968dab2c0
Kitru <pbergeman2004@gmail.com>
2013-06-05 14:36:14 -0400

    Update TankSim.py

--- TankSim.py
+++ TankSim.py
@@ -1,64 +1,157 @@
 import random
+import fileinput
 
 print("Kitru's Tank Simulator v. 1.0");
 
 iterations = int(input("How many iterations:"))
 duration = int(input("How many GCDs duration for each:"))
-tankMaxHp  = int(input("Tank's Max HP:"))
+
 global tankDef
-tankDef = float(input("Tank's Defense chance in decimal form:"))
 global tankRes
-tankRes = float(input("Tank's Resist chance in decimal form:"))
 global tankShield
-tankShield = float(input("Tank's Shield chance in decimal form:"))
 global tankAbs
-tankAbs = float(input("Tank's Absorb value in decimal form:"))
 global tankKEDR
-tankKEDR = float(input("Tank's K/E damage reduction in decimal form:"))
 global tankIEDR
-tankIEDR = float(input("Tank's I/E damage reduction in decimal form:"))
-tankSelfHeal = float(input("Tank's average self healing per GCD, if any:"))
-tankAbsShield = float(input("Tank's average damage absorb shield per GCD, if any:"))
 
-basicHit = int(input("Attacker's basic attack damage:"))
-booleanParse = input("Is the basic attack melee/ranged, T/F:")
-if booleanParse in ['true', '1', 't', 'True', 'T']:
-        basicHitMR = True
-else:
-        basicHitMR = False
-booleanParse = input("Is the basic attack kinetic/energy, T/F:")
-if booleanParse in ['true', '1', 't', 'True', 'T']:
-        basicHitKE = True
-else:
-        basicHitKE = False
+inputArray = []
 
-bigHit = int(input("Attacker's big attack damage:"))
-bigHitInterval = int(input("How many GCDs pass between big attack uses:"))
-booleanParse = input("Is the big attack melee/ranged, T/F:")
-if booleanParse in ['true', '1', 't', 'True', 'T']:
-        bigHitMR = True
-else:
-        bigHitMR = False
-booleanParse = input("Is the big attack kinetic/energy, T/F:")
-if booleanParse in ['true', '1', 't', 'True', 'T']:
-        bigHitKE = True
-else:
-        bigHitKE = False
+booleanParse = 0
+while booleanParse == 0:
+        booleanParse = input("Import Tank Stats? (Y/N)")
+        if booleanParse in ['yes', '1', 'y', 'Yes', 'Y']:
+                inputType = True
+        elif booleanParse in ['no','No','N','n','0']:
+                inputType = False
+        else:
+                print("Improper Input.")        
+booleanParse = 0
 
-basicHeal = int(input("Healers' average heal amount, per GCD:"))
-bigHeal = int(input("Healers' big heal amount, per use:"))
-bigHealInterval = int(input("How often can the big heal be used, in GCDs:"))
-booleanParse = bool(input("Is allowable overhealing quantity determined by value(T) or percent(F):"))
-if booleanParse in ['true', '1', 't', 'True', 'T']:
-	overHealType = True
-else: overHealType = False
-overHealAllowValue = 0
-overHealAllowPercentage = 0
-if overHealType == True:
-	overHealAllowValue  = int(input("How much overhealing is permissable per cast, in hp:"))
+if inputType == False:
+        tankMaxHp  = int(input("Tank's Max HP:"))
+        tankDef = float(input("Tank's Defense chance in decimal form:"))
+        tankRes = float(input("Tank's Resist chance in decimal form:"))
+        tankShield = float(input("Tank's Shield chance in decimal form:"))
+        tankAbs = float(input("Tank's Absorb value in decimal form:"))
+        tankKEDR = float(input("Tank's K/E damage reduction in decimal form:"))
+        tankIEDR = float(input("Tank's I/E damage reduction in decimal form:"))
+        tankSelfHeal = float(input("Tank's average self healing per GCD, if any:"))
+        tankAbsShield = float(input("Tank's average damage absorb shield per GCD, if any:"))
 else:
-	overHealAllowPercent = float(input("How much overhealing is permissable per cast, in percent of total heal:"))
+        inputFileName = input("Name of Tank Stat File: ")
+        for line in fileinput.input(inputFileName):
+                inputArray.append(line.rstrip())
+        tankMaxHp  = int(inputArray.pop(0))
+        tankDef = float(inputArray.pop(0))
+        tankRes = float(inputArray.pop(0))
+        tankShield = float(inputArray.pop(0))
+        tankAbs = float(inputArray.pop(0))
+        tankKEDR = float(inputArray.pop(0))
+        tankIEDR = float(inputArray.pop(0))
+        tankSelfHeal = int(inputArray.pop(0))
+        tankAbsShield = int(inputArray.pop(0))
 
+booleanParse = 0
+while booleanParse == 0:
+        booleanParse = input("Import Attacker Info? (Y/N)")
+        if booleanParse in ['yes', '1', 'y', 'Yes', 'Y']:
+                inputType = True
+        elif booleanParse in ['no','No','N','n','0']:
+                inputType = False
+        else:
+                print("Improper Input.")
+booleanParse = 0
+
+if inputType == False:
+        basicHit = int(input("Attacker's basic attack damage:"))
+        booleanParse = input("Is the basic attack melee/ranged, T/F:")
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                basicHitMR = True
+        else:
+                basicHitMR = False
+        booleanParse = input("Is the basic attack kinetic/energy, T/F:")
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                basicHitKE = True
+        else:
+                basicHitKE = False
+        bigHit = int(input("Attacker's big attack damage:"))
+        bigHitInterval = int(input("How many GCDs pass between big attack uses:"))
+        booleanParse = input("Is the big attack melee/ranged, T/F:")
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                bigHitMR = True
+        else:
+                bigHitMR = False
+        booleanParse = input("Is the big attack kinetic/energy, T/F:")
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                bigHitKE = True
+        else:
+                bigHitKE = False
+else:
+        inputFileName = input("Name of Enemy Info File: ")
+        for line in fileinput.input(inputFileName):
+                inputArray.append(line.rstrip())
+        basicHit  = int(inputArray.pop(0))
+        booleanParse = inputArray.pop(0)
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                basicHitMR = True
+        else:
+                basicHitMR = False
+        booleanParse = inputArray.pop(0)
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                basicHitKE = True
+        else:
+                basicHitKE = False
+        bigHit = int(inputArray.pop(0))
+        bigHitInterval = int(inputArray.pop(0))
+        booleanParse = inputArray.pop(0)
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                bigHitMR = True
+        else:
+                bigHitMR = False
+        booleanParse = inputArray.pop(0)
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+                bigHitKE = True
+        else:
+                bigHitKE = False
+                
+booleanParse = 0
+while booleanParse == 0:
+        booleanParse = input("Import Healer Info? (Y/N)")
+        if booleanParse in ['yes', '1', 'y', 'Yes', 'Y']:
+                inputType = True
+        elif booleanParse in ['no','No','N','n','0']:
+                inputType = False
+        else:
+                print("Improper Input.")
+booleanParse = 0
+        
+if inputType == False:
+        basicHeal = int(input("Healers' average heal amount, per GCD:"))
+        bigHeal = int(input("Healers' big heal amount, per use:"))
+        bigHealInterval = int(input("How often can the big heal be used, in GCDs:"))
+        booleanParse = input("Is allowable overhealing quantity determined by value(T) or percent(F):")
+        if booleanParse in ['true', '1', 't', 'True', 'T']:
+        	overHealType = True
+        else: overHealType = False
+        overHealAllowValue = 0
+        overHealAllowPercentage = 0
+        if overHealType == True:
+        	overHealAllowValue  = int(input("How much overhealing is permissable per cast, in hp:"))
+        else:
+        	overHealAllowPercent = float(input("How much overhealing is permissable per cast, in percent of total heal:"))
+else:
+        inputFileName = input("Name of Healer Info File: ")
+        for line in fileinput.input(inputFileName):
+                inputArray.append(line.rstrip())
+        basicHeal  = int(inputArray.pop(0))
+        bigHeal = int(inputArray.pop(0))
+        bigHealInterval = int(inputArray.pop(0))
+        overHealType = inputArray.pop(0)
+        overHealAllowValue = 0
+        overHealAllowPercentage = 0
+        if overHealType == True:
+                overHealAllowValue = int(inputArray.pop(0))
+        else:
+                overHealAllowPercent = float(inputArray.pop(0))
 
 time = 0
 dead = False
@@ -194,3 +287,5 @@ else:
 print("Number of deaths:" + repr(numDeaths))
 print("Lowest low hp: " + repr(lowestLowHp))
 print("Average lowest hp (ignoring deaths): " + repr(averageLowest))
+
+
